0b9159b293c0f9d822e43e8b97fb2746b84ff1df
davitgl <dglonti3@gmail.com>
2024-09-23 09:58:43 +0400

    Initial commit with console application

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change console background and foreground colors
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Clear to apply background color
+            Console.Clear();
+
+            // Print first and last name
+            Console.WriteLine("First Name: davit");
+            Console.WriteLine("Last Name: glonti");
+
+            // Request input from the user
+            Console.Write("Enter your input: ");
+            string userInput = Console.ReadLine();
+
+            // Print the user input
+            Console.WriteLine("You entered: " + userInput);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
